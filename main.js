3a8db3a70f290646909aa95ef68c8b461fe8c6d1
marcos <marcos@exemplo.com>
2025-02-12 21:39:34 -0300

    novo commit

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
     $('#nova-tarefa').ready(function(){
         $('#nova-tarefa').val();
     });
-  //click ou submit//
+    
     $('#botao-enviar').submit(function(){
         
 
